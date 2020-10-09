c82c1821c9dc6936872b5d7e3fb970f515d3eb1a
sarah <strongs22@up.edu>
2020-10-09 10:30:45 +1000

    Part B

--- app/src/main/java/com/example/facemaker/Face.java
+++ app/src/main/java/com/example/facemaker/Face.java
@@ -1,19 +1,56 @@
 package com.example.facemaker;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.*;
 
 /**
 @author Sarah Strong
+FALL 2020
+ Face class
+
 **/
 
-public class Face {
-    private int skinColor;
-    private int eyeColor;
-    private int hairColor;
+public class Face extends SurfaceView {
+    private boolean skinClicked;
+    private boolean eyeClicked;
+    private boolean hairClicked;
+
+     private int skinColor;
+     private int eyeColor;
+     private int hairColor;
+
     private int hairStyle;
+    private int hairRed;
+    private int hairGreen;
+    private int hairBlue;
+    private int eyesRed;
+    private int eyesGreen;
+    private int eyesBlue;
+    private int skinRed;
+    private int skinGreen;
+    private int skinBlue;
+
+
+    Paint facePaint = new Paint();
+    Paint eyePaint = new Paint();
+    Paint hairPaint = new Paint();
+    Paint whitePaint = new Paint();
+    Paint mouthPaint = new Paint();
+
 
     // Face constructor
-    public Face(){
+    public Face(Context context, AttributeSet attrs){
+        super(context,attrs);
         randomize();
+        setWillNotDraw(false);
     }
 
     /** Randomize
@@ -22,10 +59,147 @@ public class Face {
      * @return void
      **/
     public void randomize(){
-        this.skinColor = (int)(Math.random());
-        this.eyeColor = (int)(Math.random());
-        this.hairColor = (int)(Math.random());
-        this.hairStyle = (int)(Math.random());
+        this.hairStyle = (int)(Math.random()*3);
+        hairRed = (int)(Math.random()*256);
+        hairGreen = (int)(Math.random()*256);
+        hairBlue = (int)(Math.random()*256);
+        eyesRed = (int)(Math.random()*256);
+        eyesGreen = (int)(Math.random()*256);
+        eyesBlue = (int)(Math.random()*256);
+        skinRed = (int)(Math.random()*256);
+        skinGreen = (int)(Math.random()*256);
+        skinBlue = (int)(Math.random()*256);
+
+        this.skinColor = Color.rgb(skinRed, skinGreen, skinBlue);
+        this.eyeColor = Color.rgb(eyesRed, eyesGreen, eyesBlue);
+        this.hairColor = Color.rgb(hairRed, hairGreen, hairBlue);
+    }
+
+    /** onDraw
+     * Draws on canvas
+     * @param canvas
+     * @return void
+     **/
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void onDraw(Canvas canvas){
+
+        whitePaint.setColor(Color.WHITE);
+        mouthPaint.setColor(Color.RED);
+        facePaint.setColor(skinColor);
+        eyePaint.setColor(eyeColor);
+        hairPaint.setColor(hairColor);
+        facePaint.setStyle(Paint.Style.FILL);
+        eyePaint.setStyle(Paint.Style.FILL);
+        hairPaint.setStyle(Paint.Style.FILL);
+
+        drawHead(canvas);
+        drawEyes(canvas);
+        drawHair(canvas, hairStyle);
+    }
+
+    /** drawHair
+     * Helper method draws hairstyle selected from spinner
+     * @param canvas, hairStyle
+     * @return void
+     **/
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void drawHair(Canvas canvas, int hairStyle){
+        float width = canvas.getWidth();
+        float height = canvas.getHeight();
+
+        // if hairStyle == 0 bald, do nothing
+
+        if(hairStyle == 1){
+            // Short hair style
+            //canvas.drawArc(width/2-400.0f,height/2-650.0f, width/2+400.0f,
+                    //height/2-200.0f, -180,180, false, hairPaint);
+            canvas.drawRect(width/2-50.0f, height/2-700.0f,
+                    width/2+50.0f, height/2-500, hairPaint);
+
+
+        }else if(hairStyle == 2) {
+            // Long hair style
+            // Ponytail
+            canvas.drawRect(width/2, height/2+200.0f,
+                    width/2+200.0f,height/2+700, hairPaint);
+            drawHead(canvas);
+            drawEyes(canvas);
+            // Fringe
+            canvas.drawArc(width/2-450.0f,height/2-650.0f,
+                    width/2+450.0f, height/2-200.0f,
+                    -180,180, false, hairPaint);
+        }
+    }
+
+    /** drawEyes
+     * Helper method draws eyes on canvas
+     * @param canvas
+     * @return void
+     **/
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void drawEyes(Canvas canvas){
+        // Retrieve centre point of canvas
+        float centreX = canvas.getWidth()/2;
+        float centreY = canvas.getHeight()/2;
+        // Draw white part of eyes
+        canvas.drawOval(centreX-250.0f, centreY-400.0f,
+                centreX-100.0f, centreY-100.0f, whitePaint);
+        canvas.drawOval(centreX+250.0f, centreY-400.0f,
+                centreX+100.0f, centreY-100.0f, whitePaint);
+        // Draw pupils
+        canvas.drawCircle(centreX-175.0f, centreY-200.0f,
+                40, eyePaint);
+        canvas.drawCircle(centreX+175.0f, centreY-200.0f,
+                40, eyePaint);
+
+    }
+
+    /** drawHead
+     * Helper method draws head on canvas
+     * @param canvas
+     * @return void
+     **/
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void drawHead(Canvas canvas){
+        // Retrieve width and height of canvas
+        float width = canvas.getWidth();
+        float height = canvas.getHeight();
+        // Draw head
+        canvas.drawCircle(width/2, width/2, 450, facePaint);
+        // Draw mouth
+        canvas.drawArc(width/3, height/2-100.0f, 2*width/3,
+                height/2+200.0f,180, -180,
+                false,mouthPaint);
+    }
+
+    public void setHairStyle(int i){
+        hairStyle = i;
+    }
+    public void setSkinColor(int color){ skinColor = color; }
+    public void setEyeColor(int color){ eyeColor = color; }
+    public void setHairColor(int color){ hairColor = color; }
+    public int getSkinColor(){ return skinColor; }
+    public int getEyeColor(){ return eyeColor; }
+    public int getHairColor(){ return hairColor; }
+    public int getHairStyle(){ return hairStyle; }
+    public boolean isSkinClicked(){ return skinClicked; }
+    public boolean isEyeClicked(){ return eyeClicked; }
+    public boolean isHairClicked(){ return hairClicked; }
+
+    public void setSkinClicked(){
+        skinClicked = true;
+        hairClicked = false;
+        eyeClicked = false;
+    }
+    public void setHairClicked(){
+        hairClicked = true;
+        skinClicked = false;
+        eyeClicked = false;
+    }
+    public void setEyeClicked(){
+        eyeClicked = true;
+        skinClicked = false;
+        hairClicked = false;
     }
 
 }
--- app/src/main/java/com/example/facemaker/MainActivity.java
+++ app/src/main/java/com/example/facemaker/MainActivity.java
@@ -1,31 +1,174 @@
 package com.example.facemaker;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Build;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 
 /**
  @author Sarah Strong
+ * FALL 2020
+ * CS301
+ * Face Maker Project
  **/
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener,
+        RadioGroup.OnCheckedChangeListener {
+
+    private Face face;
+    private Button randomButton;
+    private RadioButton skinButton;
+    private RadioButton hairButton;
+    private RadioButton eyesButton;
+    private RadioGroup rg;
+    private SeekBar redSB;
+    private SeekBar greenSB;
+    private SeekBar blueSB;
+    private int redVal;
+    private int greenVal;
+    private int blueVal;
 
     private Spinner hairSpinner; //make Spinner object
-    String[] spinnerOptions = {"Short", "Medium", "Long"}; //options displayed on the Spinner
+    String[] spinnerOptions = {"Bald", "Mohawk", "Ponytail"}; //create options
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        face = findViewById(R.id.surfaceView);
 
+        /* External Citation
+            Date: September 14
+            Problem: Want to populate spinner
+            Resource: https://developer.android.com/guide/topics/ui/controls/spinner
+            Solution: implement code similar to example on website
+        */
         hairSpinner = (Spinner)findViewById(R.id.hairSpinner);
-
-        ArrayAdapter<String> hairAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,
-                this.spinnerOptions);
-
+        ArrayAdapter<String> hairAdapter = new ArrayAdapter<String>(
+                this,android.R.layout.simple_spinner_dropdown_item,
+                this.spinnerOptions); // Populate hairstyle spinner
+        hairAdapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
         hairSpinner.setAdapter(hairAdapter);
+        hairSpinner.setOnItemSelectedListener(this);
+
+        //Initialise button listeners
+        randomButton = findViewById(R.id.randomButton);
+
+        rg = findViewById(R.id.radioGroup);
+        rg.setOnCheckedChangeListener(this);
+        randomButton.setOnClickListener(this);
+        skinButton = findViewById(R.id.skinButton);
+        hairButton = findViewById(R.id.hairButton);
+        eyesButton = findViewById(R.id.eyesButton);
+        skinButton.setOnClickListener(this);
+        hairButton.setOnClickListener(this);
+        eyesButton.setOnClickListener(this);
+
+        // Initialise seek bar listeners
+        redSB = findViewById(R.id.redSeekBar);
+        greenSB = findViewById(R.id.greenSeekBar);
+        blueSB = findViewById(R.id.blueSeekBar);
+        redSB.setOnSeekBarChangeListener(this);
+        greenSB.setOnSeekBarChangeListener(this);
+        blueSB.setOnSeekBarChangeListener(this);
     }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        // This method gets called anytime a seekbar value gets changed
+        /* External Citation
+            Date: 1 October 2020
+            Problem: need to understand arguments of this method
+            Resource: https://developer.android.com/reference/android/widget/SeekBar.OnSeekBarChangeListener
+            Solution: realised boolean fromUser needs to be true in this case
+         */
+
+        if (fromUser) {
+            if (seekBar == redSB) {
+                redVal = progress; //update color values
+            } else if (seekBar == greenSB) {
+                greenVal = progress;
+            } else if (seekBar == blueSB) {
+                blueVal = progress;
+            }
+            // Create color given the RGB values on seekBar
+            int color = Color.rgb(redVal, greenVal, blueVal);
+
+            // Change color corresponding to checked button
+            if (hairButton.isChecked()) {
+                face.setHairColor(color);
+            } else if (eyesButton.isChecked()) {
+                face.setEyeColor(color);
+            } else if (skinButton.isChecked()) {
+                face.setSkinColor(color);
+            }
+            face.invalidate(); // always call this to redraw!
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Respond to user clicking "random face" button
+        if (view == randomButton) {
+           face.randomize();
+        }
+        face.invalidate(); // redraw
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int optioni, long l) {
+        // Set hairstyle to selected spinner item
+        face.setHairStyle(optioni);
+        // Redraw view
+        face.invalidate();
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        /* External Citation
+            Date: 2 October 2020
+            Problem: need to access individual RGB components of hair, skin and eye color ints
+            Resource: https://developer.android.com/reference/android/graphics/Color
+            Solution: found Color.red(int) method to access red component, same for GB
+         */
+        if(i == R.id.hairButton){ // When one of the radio buttons are clicked
+            face.setHairClicked(); // Update boolean isClicked value
+            //  Update seekbar progress to RGB values
+            redSB.setProgress(Color.red(face.getHairColor()));
+            greenSB.setProgress(Color.green(face.getHairColor()));
+            blueSB.setProgress(Color.blue(face.getHairColor()));
+        } else if (i == R.id.skinButton){
+            face.setSkinClicked();
+            redSB.setProgress(Color.red(face.getSkinColor()));
+            greenSB.setProgress(Color.green(face.getSkinColor()));
+            blueSB.setProgress(Color.blue(face.getSkinColor()));
+        } else if (i == R.id.eyesButton){
+            face.setEyeClicked();
+            redSB.setProgress(Color.red(face.getEyeColor()));
+            greenSB.setProgress(Color.green(face.getEyeColor()));
+            blueSB.setProgress(Color.blue(face.getEyeColor()));
+        }
+        face.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) { }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) { }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) { }
+
 
 }
